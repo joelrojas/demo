867224ec4ff9b7598d54d888c63c98657165c679
Rojas Joel <jrojas@cirrus-it.net>
2022-05-04 23:11:29 -0400

    Agregando keycloak

--- src/main/java/com/example/demo/MsSchoolApplication.java
+++ src/main/java/com/example/demo/MsSchoolApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsSchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsSchoolApplication.class, args);
+	}
+
+	@Bean
+	public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
 	}
 
 }
